762e4d18a6af8676b0a6d6f60772ce455951274f
vinoth365 <vinothpadmanaban45@gmail.com>
2024-05-22 16:43:32 +0530

    successfull

--- project/linked_list.py
+++ project/linked_list.py
@@ -2,21 +2,32 @@ from Nodefile import Node
 
 class LinkedList:
     def __init__(self) -> None:
-        pass
+        self.head = None
 
     def insert_node(self, data):
         if self.head is None:
             self.head = Node(data)
         else:
             temp = self.head
-            while temp:
+            while temp.next:
                 temp = temp.next
             temp.next = Node(data)
 
 
     def display(self):
-        pass
+        cur = self.head
+        while cur:
+            print(cur.data , end = "->")
+            cur = cur.next
+        
 
     def delete_at_begin(self):
         pass
     
+obj = LinkedList()
+obj.insert_node(1)
+obj.insert_node(2)
+obj.insert_node(3)
+obj.insert_node(4)
+obj.insert_node(5)
+obj.display()
